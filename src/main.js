d5a4eb2a7351ac54ca7e3e1ceaf3cf56a122c6d2
han yu <1807003141@qq.com>
2018-06-16 14:00:57 +0800

    应用初始化配置.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+// 解决移动端 click 事件 300毫秒延迟问题
+import fastClick = from 'fastclick';
 
-Vue.config.productionTip = false
+import './assets/styles/reset.css';
+import './assets/styles/border.css';
+
+fastClick.attach(document.body);
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +19,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
